1685e910ed48482e3f9d2109dbfce4d03c1606d6
Possible <ahunanyabenjamin@gmail.com>
2022-07-13 03:58:36 +0100

    Proxy set up 3

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,13 +1,13 @@
-const { createProxyMiddleware } = require('http-proxy-middleware')
+// const { createProxyMiddleware } = require('http-proxy-middleware')
 
 // target: 'http://127.0.0.1:5000',
-const proxy = {
-  target: 'https://pubshopapp.herokuapp.com',
-}
+// const proxy = {
+//   target: 'http://pubshopapp.herokuapp.com',
+// }
 
-module.exports = function (app) {
-  app.use('/api', createProxyMiddleware(proxy))
-}
+// module.exports = function (app) {
+//   app.use('/api', createProxyMiddleware(proxy))
+// }
 
 /* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
 // $ npm install http-proxy-middleware --save
