bb71375cc042ffeb37c0a8a56243b9a948bfc84e
Possible <ahunanyabenjamin@gmail.com>
2022-07-13 01:57:32 +0100

    Proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,13 @@
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+// target: 'http://127.0.0.1:5000',
+const proxy = {
+  target: 'https://pubshopapp.herokuapp.com',
+  changeOrigin: true,
+}
+
+module.exports = function (app) {
+  app.use('/api', createProxyMiddleware(proxy))
+}
+
+/* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
