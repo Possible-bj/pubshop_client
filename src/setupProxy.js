f041b1bc8e60185fdc39c3402e3dc32032ea9e55
Possible <ahunanyabenjamin@gmail.com>
2022-07-13 03:41:18 +0100

    Proxy set up

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -11,3 +11,6 @@ module.exports = function (app) {
 }
 
 /* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
+// $ npm install http-proxy-middleware --save
+// $ # or
+// $ yarn add http-proxy-middleware
